21cf1b0d3b8eb11d47d3a1650b9999ee6cfade6f
Suhaib Ersan <suhaib.m.ersan@gmail.com>
2021-05-03 17:02:04 +0300

    x

---
+++ js/sales.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let seattle = {
+  minCustPHour : 23,
+  maxCustPHour : 65,
+  avgCookiesPCust : 6.3,
+  activeHours : ['6am-7am','7am-8am','8am-9am','9am-10am','10am-11am','11am-12pm','12pm-1pm','1pm-2pm','2pm-3pm','3pm-4pm','4pm-5pm','5pm-6pm','6pm-7pm','7pm-8pm'],
+  randomCustPHour: [],
+
+  randomCustNumFunction: function() {
+    this.randomCustPHour.push(Math.floor(Math.random() * (this.maxCustPHour - this.minCustPHour + 1) + this.minCustPHour));
+  }
+};
+
+console.log(seattle);
