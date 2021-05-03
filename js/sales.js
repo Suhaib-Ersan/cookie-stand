430c7031d9f8ef628163e8f9affa8cf0129dd801
Suhaib Ersan <suhaib.m.ersan@gmail.com>
2021-05-03 17:29:23 +0300

    made seattle object

--- js/sales.js
+++ js/sales.js
@@ -6,10 +6,27 @@ let seattle = {
   avgCookiesPCust : 6.3,
   activeHours : ['6am-7am','7am-8am','8am-9am','9am-10am','10am-11am','11am-12pm','12pm-1pm','1pm-2pm','2pm-3pm','3pm-4pm','4pm-5pm','5pm-6pm','6pm-7pm','7pm-8pm'],
   randomCustPHour: [],
+  avgCookiesPHour: [],
 
   randomCustNumFunction: function() {
-    this.randomCustPHour.push(Math.floor(Math.random() * (this.maxCustPHour - this.minCustPHour + 1) + this.minCustPHour));
+    for (let i = 0; i < this.activeHours.length; i++) {
+      this.randomCustPHour.push(Math.floor(Math.random() * (this.maxCustPHour - this.minCustPHour + 1) + this.minCustPHour));
+    }
+  },
+  randomCookiesNumFunction: function() {
+    for (let i = 0; i < this.activeHours.length; i++) {
+      this.avgCookiesPHour.push(Math.ceil(this.randomCustPHour[i]*this.avgCookiesPCust));
+    }
+  },
+  renderFunction: function() {
+    let divEl = document.getElementById('container');
+    
   }
 };
 
-console.log(seattle);
+
+
+seattle.randomCustNumFunction();
+seattle.randomCookiesNumFunction();
+console.log(seattle.randomCustPHour);
+console.log(seattle.avgCookiesPHour);
